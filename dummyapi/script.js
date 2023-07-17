84bcff77d1a0d6c8a47f2e2ded2eca7028f4dd59
Dev <devvachhani30@gmai.com>
2023-07-17 12:50:37 +0530

    "updated"

--- dummyapi/script.js
+++ dummyapi/script.js
@@ -1,33 +1,20 @@
 "use strict";
+let count = 0;
+let totalcount = 5;
+let finaldata = [];
+async function logBlog(e) {
+  // e.preventDefault();
 
-async function logBlog() {
   try {
     const response = await fetch("https://dummyjson.com/posts");
     const data = await response.json();
 
     const blogsitems = data.posts;
     console.log(blogsitems);
-    let blogs = document.querySelector(".bitems");
-    let bloghead = document.getElementById("blogheading");
-    bloghead.innerHTML = blogsitems.title;
-    let blogbody = document.getElementById("blog-description");
-    blogbody.innerHTML = blogsitems.body;
-    let blognum = document.getElementById("blognum");
-    console.log(blognum);
+    // let blogs = document.querySelector(".bitems");
 
-    blogsitems.forEach((element) => {
-      const newDiv = `<div class="blog-items">
-      <div class="blog-title">
-        <span id="blog-id">${blognum}</span>
-        <h3 id="blogheading">${bloghead}</h3>
-        <p id="blog-description">
-         ${blogbody}
-        </p>
-      </div>
-    </div>`;
-      blogs.insertAdjacentHTML("beforeend", newDiv);
-      // console.log(blogs);
-    });
+    finaldata = blogsitems;
+    loadItems();
   } catch (error) {
     console.log(error);
   }
@@ -35,25 +22,26 @@ async function logBlog() {
 
 logBlog();
 
-function saveFormData() {
-  var name = document.getElementById("name").value;
-  var description = document.getElementById("description").value;
+function loadItems(e) {
+  // e.preventDefault();
 
-  localStorage.setItem("name", name);
-  localStorage.setItem("description", description);
-
-  alert("Form data saved successfully!");
-}
-
-function getFOrmData() {
-  const namehed = document.getElementById("nameTitle");
-  let deshead = document.getElementById("nameDescription");
-
-  var name = localStorage.getItem("name");
-  var description = localStorage.getItem("description");
-
-  namehed.innerHTML = name;
-  deshead.innerHTML = description;
-  console.log(name);
-  console.log(description);
+  let blogs = document.querySelector(".bitems");
+  if (count <= finaldata.length) {
+    for (let index = count; index <= totalcount; index++) {
+      const newDiv = `<div class="blog-items">
+      <div class="blog-title">
+        <span id="blog-id">${finaldata[index].id}</span>
+        <h3 id="blogheading">${finaldata[index].title}</h3>
+        <p id="blog-description">
+         ${finaldata[index].body}
+        </p>
+        <span id="tags"> tags: ${finaldata[index].tags}</span><br/>
+        <span id="reaction"><i class="fa fa-heart" style="font-size:18px;color:red"></i>  ${finaldata[index].reactions}</span>
+      </div>
+    </div>`;
+      blogs.insertAdjacentHTML("beforeend", newDiv);
+      count++;
+    }
+    totalcount += 6;
+  }
 }
